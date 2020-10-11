106554a1d37104056d3ed2a3ff721aa716c78316
rudigaucho <rudinei.rossales@gmail.com>
2020-10-11 18:08:32 -0300

    v1.0

---
+++ pesq_tecnico.php
@@ -0,0 +1,338 @@
+﻿<?php 
+         include "conn.php"; 
+      
+         session_start();
+
+         if(!isset($_SESSION["login"]) &&  !isset($_SESSION["senha"])  )
+            {
+                 header("Location: index.html");
+                  exit;
+            }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+
+    <link rel="icon" href="img/serede.png"> 
+<script language="Javascript">
+function confirmacao(matricula) {
+     var resposta = confirm("Deseja remover esse registro?");
+ 
+     if (resposta == true) {
+          window.location.href = "deletar_tecnico.php?matricula="+matricula;
+     }
+}
+</script>
+
+
+
+    <script type="text/javascript">
+function fnExcelReport() {
+    var tab_text = '<html xmlns:x="urn:schemas-microsoft-com:office:excel">';
+    tab_text = tab_text + '<head><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>';
+
+    tab_text = tab_text + '<x:Name>Relatorio Caixa Fechada</x:Name>';
+
+    tab_text = tab_text + '<x:WorksheetOptions><x:Panes></x:Panes></x:WorksheetOptions></x:ExcelWorksheet>';
+    tab_text = tab_text + '</x:ExcelWorksheets></x:ExcelWorkbook></xml></head><body>';
+
+    tab_text = tab_text + "<table border='1px'>";
+    tab_text = tab_text + $('#myTable').html();
+    tab_text = tab_text + '</table></body></html>';
+
+    var data_type = 'data:application/vnd.ms-excel';
+    
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+    
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+        if (window.navigator.msSaveBlob) {
+            var blob = new Blob([tab_text], {
+                type: "application/csv;charset=utf-8;"
+            });
+            navigator.msSaveBlob(blob, 'Test file.xls');
+        }
+    } else {
+        $('#test').attr('href', data_type + ', ' + encodeURIComponent(tab_text));
+        $('#test').attr('download', 'relatorio.xls');
+    }
+
+}
+
+
+
+
+
+</script> 
+    <meta name="description" content="Vali is a responsive and free admin theme built with Bootstrap 4, SASS and PUG.js. It's fully customizable and modular.">
+    <!-- Twitter meta-->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:site" content="@pratikborsadiya">
+    <meta property="twitter:creator" content="@pratikborsadiya">
+    <!-- Open Graph Meta-->
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Vali Admin">
+    <meta property="og:title" content="Vali - Free Bootstrap 4 admin theme">
+    <meta property="og:url" content="http://pratikborsadiya.in/blog/vali-admin">
+    <meta property="og:image" content="http://pratikborsadiya.in/blog/vali-admin/hero-social.png">
+    <meta property="og:description" content="Vali is a responsive and free admin theme built with Bootstrap 4, SASS and PUG.js. It's fully customizable and modular.">
+    <title>Serede</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Main CSS-->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!-- Font-icon css-->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+  </head>
+  <body class="app sidebar-mini rtl">
+    <!-- Navbar-->
+    <header class="app-header"><a class="app-header__logo" href="dashboard.php">Serede</a>
+      <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
+      <!-- Navbar Right Menu-->
+      <ul class="app-nav">
+       
+        <!--Notification Menu-->
+       
+        <!-- User Menu-->
+        <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i></a>
+          <ul class="dropdown-menu settings-menu dropdown-menu-right">
+            
+            <li><a class="dropdown-item" href="logout.php"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </header>
+    <!-- Sidebar menu-->
+    <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+    <aside class="app-sidebar">
+      <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" style="width:38px; height:40px;" src="img/serede.jpg" alt="User Image">
+        <div>
+          <p class="app-sidebar__user-name"><?php echo $_SESSION['nome'];?> </p>
+          <p class="app-sidebar__user-designation"><?php echo $_SESSION['area'];?></p>
+        </div>
+      </div>
+      <ul class="app-menu">
+      <li><a class="app-menu__item " href="dashboard.php"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+       
+        
+          <ul class="treeview-menu">
+            <li><a class="treeview-item" href="bootstrap-components.html"><i class="icon fa fa-circle-o"></i> Bootstrap Elements</a></li>
+            <li><a class="treeview-item" href="https://fontawesome.com/v4.7.0/icons/" target="_blank" rel="noopener"><i class="icon fa fa-circle-o"></i> Font Icons</a></li>
+            <li><a class="treeview-item" href="ui-cards.html"><i class="icon fa fa-circle-o"></i> Cards</a></li>
+            <li><a class="treeview-item" href="widgets.html"><i class="icon fa fa-circle-o"></i> Widgets</a></li>
+          </ul>
+        </li>
+        
+        
+        
+        
+    </aside>
+    <main class="app-content">
+      <div class="app-title">
+        <div>
+          <h1><i class="fa fa-th-list"></i> Tabela Técnicos</h1>
+          <p> </p>
+        </div>
+        <ul class="app-breadcrumb breadcrumb side">
+          <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
+          <li class="breadcrumb-item">Tabelas</li>
+          <li class="breadcrumb-item active"><a href="#">Data Table</a></li>
+        </ul>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+
+        
+          <div class="tile">
+          
+            <div class="table-responsive">
+            <form class="form-inline" role="form"   method="POST" action="pesq_tecnico.php" style="margin-left:10%;">
+    <div class="form-group">
+   
+
+    </div>
+     
+    <!-- Special version of Bootstrap that only affects content wrapped in .bootstrap-iso -->
+<link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" /> 
+
+<!--Font Awesome (added because you use icons in your prepend/append)-->
+<link rel="stylesheet" href="https://formden.com/static/cdn/font-awesome/4.4.0/css/font-awesome.min.css" />
+
+<!-- Inline CSS based on choices in "Settings" tab -->
+<style>.bootstrap-iso .formden_header h2, .bootstrap-iso .formden_header p, .bootstrap-iso form{font-family: Arial, Helvetica, sans-serif; color: black}.bootstrap-iso form button, .bootstrap-iso form button:hover{color: white !important;} .asteriskField{color: red;} </style>
+
+<!-- HTML Form (wrapped in a .bootstrap-iso div) -->
+<div style="float:left;" class="bootstrap-iso">
+
+  <div class="row">
+  
+    
+      <div class="form-group"   >  
+      
+
+      <label  for="data">
+      TR do Técnico
+      </label>
+   
+      <input type="text" class="form-control" name="tr"  required>
+      <button type="submit"   name="submit" id="submit" class="btn btn-default">Busca</button>
+  
+   </div>
+
+   </div>
+   
+
+   </div>
+   
+   
+  </form>
+ 
+   </div>
+              <table class="table table-hover table-bordered" id="myTable">
+                <thead>
+                  <tr>
+                    <th>UF</th>
+                    <th>Localidade</th>
+                    <th>Estação</th>
+                    <th>Microarea</th>
+                   
+                    
+                   
+                    
+                    
+                  </tr>
+                </thead>
+
+<?php
+  if (isset($_POST ['submit']) )
+  {
+  
+
+$tr = $_POST['tr'];
+
+
+
+
+$sql = mysql_query ("select * from localidade right join tecnico on localidade.matricula = tecnico.matricula where tecnico.matricula =   '$tr'" );
+
+
+
+
+
+    $row = mysql_num_rows($sql);
+
+ 
+ 
+
+    if (mysql_num_rows($sql) > 0)
+    {
+      while ($dado = mysql_fetch_assoc($sql))
+      
+        {
+          $nome = $dado ["nome"];
+          $ga = $dado ["nome_ga"];
+          $contato = $dado ["contato"];
+          $email = $dado ["email"];
+          $skill = $dado ["skill"];
+          $obs = $dado ["obs"];
+          $matricula = $dado ["matricula"];
+          $protocolo = $dado ["protocolo"];
+     
+      
+
+?>
+                <tbody>
+                     
+
+
+
+                  <tr>
+                    <td><?php echo $dado ["uf"];  ?></td>
+                    <td><?php echo $dado ["localidade"];  ?></td>
+                    <td><?php echo $dado ["estacao"];  ?></td>
+                    <td><?php echo $dado ["micro"];  ?></td>
+                   
+                    
+                    
+                   
+
+
+
+                  </tr>
+
+                  
+<!-- Modal -->
+
+      <?php 
+      
+                      
+      }   ?>
+      
+      <div class="page-header">
+      <fieldset style="border: 2px groove; padding:2px;">
+      <label>Nome:</label> <?php echo $nome;  ?><br>
+      <label>Skill:</label><?php echo $skill;  ?><br>
+      <label>Contato:</label><?php echo $contato;  ?><br>
+      <label>GA:</label><?php echo $ga;  ?><br>
+      
+      <label>Nascimento:</label><?php echo $email;  ?><br>
+      <label>Obs:</label><?php echo $obs;  ?><br>
+      <?php if($_SESSION['acesso'] == 'CL_ADM') {?>
+      <div class="form-group" style="float:right;">
+      <a href="editar_tecnico.php?matricula=<?php echo $matricula;?> "
+     class="btn btn-info btn-sm active" role="button" aria-pressed="true">Editar</a>  
+      <a href="javascript:func()"
+     onclick="confirmacao('<?php echo $matricula;?>')" class="btn btn-danger btn-sm active" role="button" aria-pressed="true">Deletar</a>
+     
+     </div>
+     <?php }?>
+      </fieldset>
+      
+               </div>
+         
+               <?php } }?>
+                </tbody>
+              </table>
+            
+            
+          </div>
+          
+        </div>
+        
+      </div>
+    </main>
+
+    
+    
+    <!-- Essential javascripts for application to work-->
+
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    <!-- The javascript plugin to display page loading on top-->
+    <script src="js/plugins/pace.min.js"></script>
+    <!-- Page specific javascripts-->
+    <!-- Data table plugin-->
+    <script type="text/javascript" src="js/plugins/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="js/plugins/dataTables.bootstrap.min.js"></script>
+    <script type="text/javascript">$('#sampleTable').DataTable();</script>
+    <!-- Google analytics script-->
+    <script type="text/javascript">
+      if(document.location.hostname == 'pratikborsadiya.in') {
+      	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      	ga('create', 'UA-72504830-1', 'auto');
+      	ga('send', 'pageview');
+      }
+    </script>
+
+  
+  </body>
+</html>
